0417490f474bf93d049e3af158bc369dc245dc2e
Jeremy Jie Huang <jh3627@stern.nyu.edu>
2015-01-05 14:00:38 -0500

    Initial Commit

---
+++ DormCatChat/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DormCatChat
+//
+//  Created by Huang Jie on 1/5/15.
+//  Copyright (c) 2015 Huang Jie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
